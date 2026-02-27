6420634bce97220e63a7e02bfae3fc0b7dcc1f10
pablo-angosr <pangbo@affine.io>
2026-02-27 03:13:52 +0000

    Skip hybrid_cross_domain_calc when weather plugin is disabled

--- liveweb_arena/plugins/hybrid/templates/__init__.py
+++ liveweb_arena/plugins/hybrid/templates/__init__.py
@@ -1,12 +1,13 @@
 """Hybrid cross-site query templates"""
 
+from liveweb_arena.plugins import DISABLED_PLUGINS
+
 from .top_performer import HybridTopPerformerTemplate
 from .ranking import HybridRankingTemplate
 from .conditional_branch import HybridConditionalBranchTemplate
 from .portfolio import HybridPortfolioRebalanceTemplate
 from .anomaly import HybridAnomalyDetectionTemplate
 from .chained_decision import HybridChainedDecisionTemplate
-from .cross_domain_calc import HybridCrossDomainCalcTemplate
 from .satisficing_search import HybridSatisficingSearchTemplate
 
 __all__ = [
@@ -16,6 +17,10 @@ __all__ = [
     "HybridPortfolioRebalanceTemplate",
     "HybridAnomalyDetectionTemplate",
     "HybridChainedDecisionTemplate",
-    "HybridCrossDomainCalcTemplate",
     "HybridSatisficingSearchTemplate",
 ]
+
+# cross_domain_calc depends on weather plugin
+if "weather" not in DISABLED_PLUGINS:
+    from .cross_domain_calc import HybridCrossDomainCalcTemplate
+    __all__.append("HybridCrossDomainCalcTemplate")
